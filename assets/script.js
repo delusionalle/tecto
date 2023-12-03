d09730b15798335ae514b6e9f387177ef5a0872f
delusionalle <antiiiclimactical@gmail.com>
2023-12-03 16:16:29 +0600

    короче sidebar nav

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Get the map section element
-    var mapSection = document.getElementById("mapSection");
-
-    // Get the button that opens the map
-    var openMapButton = document.getElementById("openMapButton");
-
-    // When the user clicks on the button, show the map section and hide other sections
-    openMapButton.addEventListener("click", function () {
-        mapSection.classList.toggle("hidden");
-        hideOtherSections("mapSection");
-    });
-
-    // Function to hide other sections when showing a specific section
-    function hideOtherSections(sectionToShow) {
-        var sections = document.querySelectorAll("#main-content > div");
-        sections.forEach(function (section) {
-            if (section.id !== sectionToShow) {
-                section.classList.add("hidden");
-            }
-        });
-    }
-});
+function toggleMenu() {
+    var menuContainer = document.getElementById("menuContainer");
+    menuContainer.classList.toggle("active");
+}
